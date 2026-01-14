c05aed8ed758627e34c5a10457d6d6ebe6cd8746
userName <example@email.com>
2026-01-14 17:01:11 +0100

    Portfolio Project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Personal Portfolio
+
+This is my personal portfolio website created to showcase my projects, skills, and experience.  
+It serves as a central place where visitors can explore my work and learn more about me as a developer.
+
+## Live Demo
+
+[em-personalportfolio.vercel.app/](https://em-personalportfolio.vercel.app/)
+
+## Features
+
+-   Display of personal projects with preview images
+-   Project details such as technologies and links
+-   Responsive design for different screen sizes
+-   Clean and minimal UI
+
+## Technologies Used
+
+-   **HTML** – Structure of the website
+-   **CSS** – Styling and layout
+-   **JavaScript** – Interactivity and dynamic content
+-   **Supabase** – Used as a backend service to store:
+    -   Project data
+    -   Preview images for the projects
---
+++ script.js
@@ -0,0 +1,137 @@
+const container = document.getElementById("projects");
+const buttons = document.querySelectorAll(".allButton, .hcButton, .jsButton");
+let currentFilter = "all";
+
+let projects = [];
+
+const sb = window.supabase.createClient(
+    "https://ihwaxmhsanxznaopnvtz.supabase.co",
+    "sb_publishable_cAAIiJW63lR-7NG99-RnkQ__mnu4EVJ"
+);
+
+async function loadProjects() {
+    const { data, error } = await sb.from("projects").select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    projects = data;
+
+    renderProjects();
+}
+
+function renderProjects() {
+    container.innerHTML = "";
+
+    let filteredProjects = [...projects];
+
+    if (currentFilter === "htmlcss") {
+        filteredProjects = projects.filter((px) => px.tag === "HTML / CSS");
+    } else if (currentFilter === "javascript") {
+        filteredProjects = projects.filter((px) => px.tag === "JavaScript");
+    }
+
+    filteredProjects
+        .sort((a, b) => a.id_column - b.id_column)
+        .forEach((p) => {
+            createProjectCard(p);
+        });
+}
+
+function createProjectCard(p) {
+    const div = document.createElement("div");
+    div.className = "project";
+
+    const dbDate = p.created_at;
+
+    const date = new Date(dbDate);
+
+    const formatted = date.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+
+    const img = document.createElement("img");
+    img.src = `${p.image_url}`;
+    img.alt = `${p.name}`;
+
+    const textWrapper = document.createElement("div");
+    textWrapper.classList.add("textWrapper");
+
+    const label = document.createElement("div");
+    label.classList.add("label");
+
+    const tag = document.createElement("p");
+    tag.classList.add("tag");
+    p.tag === "JavaScript"
+        ? tag.classList.add("javascript")
+        : tag.classList.add("htmlcss");
+    tag.textContent = `${p.tag}`;
+
+    const formattedDate = document.createElement("p");
+    formattedDate.textContent = `${formatted}`;
+
+    label.append(tag, formattedDate);
+
+    const projectName = document.createElement("p");
+    projectName.classList.add("name");
+    projectName.textContent = `${p.name}`;
+
+    const links = document.createElement("div");
+    links.classList.add("url");
+    links.append(
+        createLink(p.url, "./images/globe.svg", "globe icon"),
+        createLink(p.github, "./images/github-logo.svg", "github icon"),
+        createLink(
+            p.sourceurl,
+            "./images/FrontEndMentor.png",
+            "fem logo",
+            "Frontend Mentor"
+        )
+    );
+    textWrapper.append(label, projectName, links);
+
+    div.append(img, textWrapper);
+
+    container.appendChild(div);
+}
+
+function createLink(href, imgSrc, alt, title) {
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = href;
+    a.target = "_blank";
+    a.rel = "noopener noreferred";
+
+    img.src = imgSrc;
+    img.alt = alt;
+    if (title) {
+        img.title = title;
+    }
+
+    a.appendChild(img);
+
+    return a;
+}
+
+function setActiveButton(clickedBtn, filter) {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+
+    clickedBtn.classList.add("active");
+
+    currentFilter = filter;
+
+    renderProjects();
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        setActiveButton(btn, btn.dataset.filter);
+    });
+});
+
+loadProjects();
